698cc8ae648cc5146a0190756f0aa08ff34cd183
dmitry <bdodinka1992@gmail.com>
2023-01-19 13:02:35 +0300

    fix:test App,InitApp

--- src/App/App.test.tsx
+++ src/App/App.test.tsx
@@ -1,12 +1,37 @@
 import React from 'react'
 
 import { render, screen } from '@testing-library/react'
+import { Provider } from 'react-redux'
 
 import App from './App'
+import { InitializeAppTC } from './appReducer'
+import { store } from './store'
 
-test('renders learn react link', () => {
-  render(<App />)
-  const linkElement = screen.getByText(/learn react/i)
+describe('App ', () => {
+  it('render App', () => {
+    render(
+      <Provider store={store}>
+        <App />
+      </Provider>
+    )
+    expect(screen.getByText(/Render/i)).toBeInTheDocument()
+  })
+})
 
-  expect(linkElement).toBeInTheDocument()
+describe('initialize App', () => {
+  beforeEach(async () => {
+    await store.dispatch(InitializeAppTC())
+  })
+
+  it('App isWorking ', () => {
+    const isErr = store.getState().app.ErrorMessage
+
+    render(
+      <Provider store={store}>
+        <App />
+      </Provider>
+    )
+    expect(screen.getByRole('main')).toBeInTheDocument()
+    expect(isErr).toBeNull()
+  })
 })
