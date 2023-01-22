861e0b8575d73589ed45f7fb5fed1a3bc894e9e8
DMITRY <bdodinka1992@gmail.com>
2023-01-22 17:52:03 +0300

    feat:add component Crash with style

--- src/features/Crash/Crash.tsx
+++ src/features/Crash/Crash.tsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+import { Wrapper } from '../../common/wrapper/Wrapper'
+
+import style from './Crash.module.scss'
 
 type PropsType = {
   errorMessage: string | null
 }
 
 export const Crash = (props: PropsType) => {
-  return <div>{props.errorMessage}</div>
+  const [showError, setShowError] = useState<boolean>(false)
+
+  const onFocusMouseHandler = (event: React.MouseEvent<HTMLDivElement>) => {
+    event && setShowError(true)
+  }
+  const onLeaveMouseHandler = (event: React.MouseEvent<HTMLDivElement>) => {
+    event && setShowError(false)
+  }
+
+  return (
+    <div className={style.main}>
+      <Wrapper>
+        <div className={style.content}>
+          <div className={style.item}></div>
+          <div className={style.item_cats}></div>
+          <div
+            className={style.error}
+            onMouseEnter={onFocusMouseHandler}
+            onMouseLeave={onLeaveMouseHandler}
+          >
+            {showError && 'ssssss'}
+          </div>
+        </div>
+      </Wrapper>
+    </div>
+  )
 }
